10c47a5e1cea708f676c132e41ea9998c3f097b6
emdneto <9735060+emdneto@users.noreply.github.com>
2025-02-13 22:20:48 -0300

    fixes
    Signed-off-by: emdneto <9735060+emdneto@users.noreply.github.com>

--- src/python/.template/devenv.nix
+++ src/python/.template/devenv.nix
@@ -1,16 +1,16 @@
 {pkgs, ...}:
 {
-    processes.snippet.exec = "opentelemetry-instrument flask --app snippet_zero.py run -p 5001";
-    processes.snippet.process-compose.environment = [  "OTEL_SERVICE_NAME=flask-zero-code" ];
+    # processes.snippet.exec = "opentelemetry-instrument flask --app snippet.py run -p 5001";
+    # processes.snippet.process-compose.environment = [  "OTEL_SERVICE_NAME=flask-zero-code" ];
 
     enterShell = ''
-        uv pip install -r requirements.txt
-        opentelemetry-bootstrap -a install
+        echo "entering shell"
+        # uv pip install -r requirements.txt
+        # opentelemetry-bootstrap -a install
     '';
 
     enterTest = ''
-        wait_for_port 5001
-        curl --fail http://localhost:5001/rolldice
-        curl http://localhost:5001/404
+        wait_for_port 8080
+        ${pkgs.curl}/bin/curl --fail http://localhost:8080/telemetry
     '';
 }
