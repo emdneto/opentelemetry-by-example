b989d92afdb57b4a995dd849dbae2b57719f6bba
emdneto <9735060+emdneto@users.noreply.github.com>
2025-02-13 23:49:30 -0300

    fastapi zero-code
    Signed-off-by: emdneto <9735060+emdneto@users.noreply.github.com>

---
+++ src/python/fastapi/devenv.nix
@@ -0,0 +1,15 @@
+{pkgs, ...}:
+{
+    processes.snippet_uvicorn.exec = "opentelemetry-instrument uvicorn snippet_uvicorn:app --port 3000 --workers 1";
+    processes.snippet_uvicorn.process-compose.environment = [  "OTEL_SERVICE_NAME=fastapi-uvicorn-zero-code" ];
+
+    enterShell = ''
+        uv pip install -r requirements.txt
+        opentelemetry-bootstrap -a requirements | uv pip install --requirement -
+    '';
+
+    enterTest = ''
+        wait_for_port 3000
+        ${pkgs.curl}/bin/curl --fail http://localhost:3000/foobar
+    '';
+}
---
+++ src/python/fastapi/tests/test_snippet.py
@@ -0,0 +1,50 @@
+from snippet_test_utils import SnippetTestBase
+
+
+class Snippet(SnippetTestBase):
+    def test_snippet_zero_code(self):
+        spans = self.get_spans("fastapi-uvicorn-zero-code")
+        self.assertEqual(len(spans), 3)
+        # present_attributes = [
+        #     "net.host.name",
+        #     "net.peer.port",
+        #     "http.user_agent",
+        #     "net.peer.ip",
+        # ]
+
+        # strict_match_attributes = {
+        #     "http.method": "GET",
+        #     "http.server_name": "127.0.0.1",
+        #     "http.scheme": "http",
+        #     "http.host": "localhost:5001",
+        #     "net.host.port": 5001,
+        #     "http.target": "/rolldice",
+        #     "http.flavor": "1.1",
+        #     "http.route": "/rolldice",
+        #     "http.status_code": 200,
+        # }
+
+        # span = spans[0]
+        # self.assert_span(
+        #     span,
+        #     span_name="GET /rolldice",
+        #     kind="SERVER",
+        #     scope_name="opentelemetry.instrumentation.flask",
+        #     attributes=strict_match_attributes,
+        # )
+        # self.assert_attributes_present(
+        #     span.get("attributes", {}), present_attributes
+        # )
+
+        # error_attributes = {
+        #     "http.method": "GET",
+        #     "http.status_code": 404,
+        # }
+
+        # span_error = spans[1]
+        # self.assert_span(
+        #     span_error,
+        #     span_name="GET /404",
+        #     kind="SERVER",
+        #     attributes=error_attributes,
+        # )
